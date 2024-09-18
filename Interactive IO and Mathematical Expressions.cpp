29836a7912c8e4629254f8d17f676654c04f91e8
orionBlazey <157438895+orionBlazey@users.noreply.github.com>
2024-09-18 12:04:12 -0400

    This is the Interest Earned branch

--- Interactive IO and Mathematical Expressions.cpp
+++ Interactive IO and Mathematical Expressions.cpp
@@ -3,26 +3,9 @@
 
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-    const double conversion = 0.021;
-    const double sugar = 1.5;
-    const double butter = 1;
-    const double flour = 2.75;
-    double totSugar, totButter, totFlour, cookies, ingAmount;
-
-    cout << "How many cookies would you like to bake?: ";
-    cin >> cookies;
-    
-    ingAmount = cookies * conversion;
-    totSugar = ingAmount * sugar;
-    totButter = ingAmount * butter;
-    totFlour = ingAmount * flour;
-
-    cout << "To bake " << cookies << " cookies, you need:\n";
-    cout << fixed << setprecision(2) << showpoint;
-    cout << totSugar << " cups of sugar\n" << totButter << " cups of butter\n" << totFlour << " cups of flour\n" << endl;
+    double principal, rate, interest, compound, amount, rateCompound;
 }
